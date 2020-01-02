XayahSuSuSu/orangefox_bootable_recovery

commit ea898ed3821237f2b72047ea00ea041fb9f403c0

Try to avoid spontaneous reboots to fastboot mode when using custom encryption

twrp-functions.cpp
@@ -279,6 +279,8 @@ bool i = Path_Exists("/tmp/orangefox.cfg");
 /* rerun startup if needed after decryption */
 bool TWFunc::Rerun_Startup(void)
 {
+   return false; // something is causing spontaneous reboots to fastboot, so just exit
+   
    if (OrangeFox_Startup_Executed > 0)
       return false;
 

commit 7fe3a071bcac12af0f0e3edcb7497f9008794f30

Fix fancy decryption page * DARTHJABBA9 TRIGGERED * Originally introduced in f7c927e8 then broken in ea898ed3 Changes: * `data_decrypted` var is removed from c++ code. Now it will be set in xml * removed `used_custom_encryption` var because it does nothing * removed gui_loadCustomResources() because we reload theme in xml files

gui/theme/portrait_hdpi/pages/mount.xml
@@ -340,6 +340,7 @@
 			<patternpassword>
 				<placement x="%pattern_x%" y="%row4_1a_y%" w="%pattern_size%" h="%pattern_size%"/>
 				<data name="tw_crypto_password"/>
+				<action function="set">data_decrypted=0</action>
 				<action function="page">trydecrypt</action>
 			</patternpassword>
 
@@ -409,10 +410,28 @@
 				<action function="page">decrypt</action>
 			</action>
 
+			<!-- After successful decryption at recovery startup, here [vvv]
+				 we set data_decrypted to 1, that triggers block of code
+				 in twrp.cpp [DataManager::GetStrValue("data_decrypted") == "1"]
+				 that instead of starting UI from `main` page,
+				 requesting reload and starting UI from `reapply_settings` page.
+				 Recovery shows up `reapply_settings` and begin reload theme.
+				 When theme was reloaded, recovery shows up `main` page,
+				 then `filemanagerlist` page. -->
+
 			<action>
 				<condition var1="tw_operation_state" var2="1"/>
 				<condition var1="tw_operation_status" var2="0"/>
+				<action function="set">data_decrypted=1</action>
 				<action function="set">tw_page_done=1</action>
+				<!-- next code will execute after user cancelled decryption at startup
+				     and later selected decrypt menu item
+					 This happens because block of code in twrp.cpp can't execute in this case.
+					 Why I can't remove block of code in twrp.cpp? Because twrp
+					 calls `decrypt` page and if we just reload after
+					 decryption using `action function` we will broke twrp startup code.
+					 We need to exit from `decrypt` page using `tw_page_done`
+					 to continue startup. -->
 				<action function="set">of_reload_back=main</action>
 				<action function="reload"/>
 				<action function="page">reapply_settings</action>

twrp-functions.cpp
@@ -334,7 +334,6 @@ bool TWFunc::Rerun_Startup(void)
    if (i == true || tprop == "1")
      return false;
 
-   DataManager::SetValue("data_decrypted", "1");
    // LOGINFO("OrangeFox: Reading settings file - again...\n");
    DataManager::ReadSettingsFile();
 

twrp.cpp
@@ -274,7 +274,6 @@ int main(int argc, char **argv)
 	LOGINFO("Backup of TWRP ramdisk done.\n");
 #endif
 
-	DataManager::SetValue("data_decrypted", "0");
 	// Offer to decrypt if the device is encrypted
 	if (DataManager::GetIntValue(TW_IS_ENCRYPTED) != 0) {
 		if (SkipDecryption) {
@@ -287,7 +286,6 @@ int main(int argc, char **argv)
 				  LOGINFO("- DEBUG: OrangeFox OTA: detected custom encryption\n");
 				  DataManager::SetValue("OTA_decrypted", "1");
 				  TWFunc::check_selinux_support();
-				  gui_loadCustomResources();
 			       } 
 			 }
 			else //
@@ -297,7 +295,6 @@ int main(int argc, char **argv)
 			LOGINFO("Is encrypted, do decrypt page first\n");			
 			//
 			LOGINFO("- DEBUG: OrangeFox: detected custom encryption\n");
-			DataManager::SetValue("used_custom_encryption", "1");
 			usleep(16);
 			//
 			if (gui_startPage("decrypt", 1, 1) != 0) {
@@ -305,7 +302,6 @@ int main(int argc, char **argv)
 			} else {
 				// Check for and load custom theme if present
 				TWFunc::check_selinux_support();
-				gui_loadCustomResources();
 			}
 		}
 	} else if (datamedia) {
@@ -413,7 +409,7 @@ int main(int argc, char **argv)
   // LOGINFO("OrangeFox: Reloading theme to apply generated theme on sdcard - again...\n");
   if (DataManager::GetStrValue("data_decrypted") == "1")
   {
-	//[f/d] Start UI using reapply_settings page
+	//[f/d] Start UI using reapply_settings page (executed on recovery startup)
   	DataManager::SetValue("of_reload_back", "main");
 	PageManager::RequestReload();
     gui_startPage("reapply_settings", 1, 0);