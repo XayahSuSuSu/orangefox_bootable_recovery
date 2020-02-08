206c418154e22b5569f753d16c9222070405e637
Darth9 <reyprimerjefe@gmail.com>
2020-02-08 21:26:38 +0000

    Add ozip decryption for Oppo/Realme device - @mauronofrio commit fd730284d16ad23319b9b78afaa9bf5776537f81

--- Android.mk
+++ Android.mk
@@ -1124,6 +1124,11 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 24; echo $$?),0)
     include $(commands_TWRP_local_path)/libmincrypt/Android.mk
 endif
 
+ifneq ($(TW_OZIP_DECRYPT_KEY),)
+    TWRP_REQUIRED_MODULES += ozip_decrypt
+    include $(commands_TWRP_local_path)/ozip_decrypt/Android.mk
+endif
+
 ifeq ($(TW_INCLUDE_CRYPTO), true)
     ifeq ($(OF_USE_LEGACY_CRYPTO),1)
     include $(commands_TWRP_local_path)/crypto/lollipop/Android.mk
