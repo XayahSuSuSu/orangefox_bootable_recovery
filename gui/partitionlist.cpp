2c5eabfcb4fc93f5aec3defcf07550a54ccdaa4f
fordownloads <vnukov.d03@gmail.com>
2020-05-16 03:27:52 +0800

    Move storage selection to quick menu on top * This is workaround for storage selection bug

--- gui/partitionlist.cpp
+++ gui/partitionlist.cpp
@@ -315,7 +315,6 @@ void GUIPartitionList::NotifySelect(size_t item_selected)
 					mUpdate = 1;
 
 					DataManager::SetValue(mVariable, str);
-					DataManager::SetValue("tw_file_location1", str);
 				}
 			} else {
 				if (ListType == "flashimg" || ListType == "part_option" ) { // only one item can be selected for flashing images
