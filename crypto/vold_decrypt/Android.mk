8b55a33c72635c4aa9d52b203bfec0dd2ba64210
Darth9 <reyprimerjefe@gmail.com>
2020-05-21 16:13:49 +0100

    vold_decrypt: add missing includes for ext4 policies; commit 5db50ded70efcea4fe8286689a764860861fdc16

--- crypto/vold_decrypt/Android.mk
+++ crypto/vold_decrypt/Android.mk
@@ -115,6 +115,12 @@ ifeq ($(TW_INCLUDE_CRYPTO), true)
 
         LOCAL_SRC_FILES = vold_decrypt.cpp
         LOCAL_SHARED_LIBRARIES := libcutils
+        LOCAL_C_INCLUDES += system/extras/ext4_utils/include
+        ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 29; echo $$?),0)
+            LOCAL_C_INCLUDES += bootable/recovery/crypto/fscrypt
+        else
+            LOCAL_C_INCLUDES += bootable/recovery/crypto/ext4crypt
+        endif
         include $(BUILD_STATIC_LIBRARY)
 
         ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 28; echo $$?),0)
