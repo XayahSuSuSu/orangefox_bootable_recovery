5f78d164420479dc41e9c88067381424ea692441
Captain_Throwback <captainthrowback@hotmail.com>
2020-10-26 11:21:20 +0000

    init: include more services in TWRP by default

--- Android.mk
+++ Android.mk
@@ -148,7 +148,7 @@ endif
 ifeq ($(AB_OTA_UPDATER),true)
     LOCAL_CFLAGS += -DAB_OTA_UPDATER=1
     LOCAL_SHARED_LIBRARIES += libhardware android.hardware.boot@1.0
-    TWRP_REQUIRED_MODULES += libhardware
+    TWRP_REQUIRED_MODULES += libhardware android.hardware.boot@1.0-service android.hardware.boot@1.0-service.rc
 endif
 
 ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS),true)
@@ -392,22 +392,24 @@ TWRP_REQUIRED_MODULES += \
     init.recovery.hlthchrg.rc \
     init.recovery.service.rc \
     init.recovery.ldconfig.rc \
+    hwservicemanager \
     hwservicemanager.rc \
+    servicemanager \
     servicemanager.rc \
     awk \
     toybox \
     toolbox \
     mkshrc_twrp \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service \
+    android.hardware.health@2.0-service.rc
 
 ifneq ($(TW_INCLUDE_CRYPTO),)
 TWRP_REQUIRED_MODULES += \
     plat_service_contexts \
     plat_hwservice_contexts \
     vendor_hwservice_contexts \
-    hwservicemanager \
-    servicemanager \
     vndservicemanager \
+    vndservicemanager.rc \
     vold_prepare_subdirs \
     task_recovery_profiles.json \
     fscryptpolicyget
