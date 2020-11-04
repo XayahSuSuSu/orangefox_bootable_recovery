965ed4df883b90b822bcc0c056241f14508e4671
Darth9 <reyprimerjefe@gmail.com>
2020-11-04 22:42:58 +0000

    Stop spurious spamming about fstab and misc

--- bootloader_message/bootloader_message.cpp
+++ bootloader_message/bootloader_message.cpp
@@ -46,7 +46,7 @@ static std::string get_misc_blk_device(std::string* err) {
   }
   Fstab fstab;
   if (!ReadDefaultFstab(&fstab)) {
-    printf("failed to read default fstab\n");
+  /*  printf("failed to read default fstab\n"); */
     *err = "failed to read default fstab";
     return "";
   }
@@ -57,7 +57,7 @@ static std::string get_misc_blk_device(std::string* err) {
   }
 
   *err = "failed to find /misc partition";
-  printf("failed to find misc partition\n");
+/*  printf("failed to find misc partition\n"); */
   return "";
 }
 
