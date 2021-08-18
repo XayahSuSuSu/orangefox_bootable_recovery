6979b0f946433b591b2dcfa10c37cec38111b3d3
Darth9 <reyprimerjefe@gmail.com>
2021-08-18 14:33:37 +0100

    Use '/super' instead of '/super_image' for OTA

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -3633,7 +3633,7 @@ int TWPartitionManager::Run_OTA_Survival_Backup(bool adbbackup)
     part_settings.Backup_Folder + "/" + Backup_Name;
 
   if (TWFunc::Has_Dynamic_Partitions()) 
-  	theSystem = "/super_image";
+  	theSystem = "/super";
   else
   	theSystem = "/system_image";
     
